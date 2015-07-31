2d01c20e1b864cda86e581dfec473609b9baf898
Nathan A. Pacelli <nathan@pacelli.co>
2015-07-30 21:46:06 -0400

    Updates

--- App/evoluteJS.js
+++ App/evoluteJS.js
@@ -4,25 +4,25 @@ $(document).ready(function(e) {
     
 	$("nav").mouseenter(function() {
     	$(this).stop().animate({
-        	width: "250px"
+        	width: "200px"
     	}, 100 );
 		$('.header-icon img, .logo-icon img').stop().animate({
-			top: '-60px'
+			top: '-40px'
 			}, 0 );
 	});
 	
 	$(".logo-icon, .header-icon").mouseenter(function() {
     	$("nav").stop().animate({
-        	width: "250px"
+        	width: "200px"
     	}, 100 );
 		$('.header-icon img, .logo-icon img').stop().animate({
-			top: '-60px'
+			top: '-40px'
 			}, 0 );
 	});
 	
 	$("nav").mouseleave(function() {
     	$(this).stop().animate({
-        	width: "80px"
+        	width: "60px"
     	}, 100 );
 		$('.header-icon img, .logo-icon img').stop().animate({
 			top: '0'
@@ -31,7 +31,7 @@ $(document).ready(function(e) {
 	
 	$("nav").click(function() {
     	$(this).stop().animate({
-        	width: "80px"
+        	width: "60px"
     	}, 100 );
 		$('.header-icon img, .logo-icon img').stop().animate({
 			top: '0'
@@ -43,6 +43,7 @@ $(document).ready(function(e) {
 
 var evoluteApp = angular.module('evoluteApp', ['ngRoute']);
 	
+	//routes - provide navigation
 evoluteApp.config(function($routeProvider) {
         $routeProvider
 
@@ -86,6 +87,21 @@ evoluteApp.config(function($routeProvider) {
             .when('/configuration', {
                 templateUrl : 'pages/configuration.html',
                 controller  : 'configurationController'
+            })
+			
+			.when('/service', {
+                templateUrl : 'pages/service.html',
+                controller  : 'serviceController'
+            })
+			
+			.when('/hostRing', {
+                templateUrl : 'pages/hostRingName.html',
+                controller  : 'hostRingController'
+            })
+			
+			.when('/servicePerformance', {
+                templateUrl : 'pages/servicePerformance.html',
+                controller  : 'servicePerformanceController'
             });
     });
 	
@@ -93,42 +109,93 @@ evoluteApp.config(function($routeProvider) {
     evoluteApp.controller('dashController', function($scope) {
 
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
     });
 	
 	evoluteApp.controller('appsController', function($scope) {
 
         // create a message to display in our view
-        $scope.message = 'Everyone come my apps';
     });
 	
-	evoluteApp.controller('containerController', function($scope) {
+	evoluteApp.controller('containerController', function($scope, $http) {
+      
+	  'use strict';
+  		//minifcation proof by explicitly including in inject array
+		
+			// $scope.data = 'the data'; //display this using {{data}} in the view
+			//dont need to inject scope with this
+			// this.data = 'the data';
+			var vm = this;
+			
+			$http.get('http://104.236.162.244:4243/containers/json?all=1')
+			.then(function(response) {
+				vm.containers = response.data;
+			},
+			function(reason) {
+				console.log(reason);
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
 
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see my containers';
     });
 	
 	evoluteApp.controller('imagesController', function($scope) {
 
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
     });
 	
 	evoluteApp.controller('schedulerController', function($scope) {
 
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
     });
 	
 	evoluteApp.controller('revisionsController', function($scope) {
 
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
     });
 	
 	evoluteApp.controller('configurationController', function($scope) {
 
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
     });
+	
+	evoluteApp.controller('serviceController', function($scope) {
+
+        // create a message to display in our view
+    });
+	
+	evoluteApp.controller('hostRingController', function($scope) {
+
+        // create a message to display in our view
+    });
+	
+	evoluteApp.controller('servicePerformanceController', function($scope) {
+
+        // create a message to display in our view
+    });
+	
+    evoluteApp.controller('appFormController', ['$scope', function($scope) {
+      $scope.master = {};
+	  
+	  $scope.conImage = "Container Image"
+	  $scope.variable = "Variables"
+	  $scope.init = "Init Command"
+	  
+	  $scope.appName = { id: null };
+		$scope.appNames = [
+			{ id: 0, name: 'App 1' }, 
+			{ id: 1, name: 'App 2' }, 
+			{ id: 2, name: 'App 3' }
+		];
+
+      $scope.update = function(app) {
+        $scope.master = angular.copy(app);
+      };
+
+      $scope.reset = function() {
+        $scope.app = angular.copy($scope.master);
+      };
+
+      $scope.reset();
+    }]);
 	
